03b874630be5384cd7989e4e4143d6a660fe5dab
Ruben Becker <79900846+rbnbckr@users.noreply.github.com>
2025-11-12 16:32:50 +0100

    Initial import

---
+++ earthquakes.py
@@ -0,0 +1,24 @@
+import requests
+import datetime
+import json
+
+
+USGS_URL = 'https://earthquake.usgs.gov/fdsnws/event/1/query?starttime={}&format=geojson&limit=20000'
+
+def get_earthquake(days_past):
+    #get the date of today - days_past days at 00 AM
+    start_date = (datetime.datetime.now() + datetime.timedelta(days=-days_past)).strftime("%Y-%m-%d")
+    url = USGS_URL.format(start_date)
+    r = requests.get(url)
+    events = json.loads(requests.get(url).text)
+    magnitude = 0
+    place = ''
+    for event in events['features']:
+        try:
+            mag = float(event['properties']['mag'])
+        except TypeError:
+            pass
+        if mag > magnitude:
+            magnitude = mag
+            place = event['properties']['place']
+    return magnitude, place
---
+++ main.py
@@ -0,0 +1,6 @@
+from earthquakes import get_earthquake
+
+days = 10
+mag, place = get_earthquake(days)
+
+print("The largest earthquake of last {} days had magnitude {} and was located at {}".format(days, mag, place))
