8323e2884c58c18fe742fa8e45006b5549fd6070
giuliabordignon <894116@stud.unive.it>
2026-01-31 17:27:59 +0100

    Step 6: query database and print earthquakes

--- earthquakes/earthquakes.py
+++ earthquakes/earthquakes.py
@@ -27,7 +27,6 @@ import csv
 import requests
 from datetime import datetime, timedelta
 
-
 def read_bounding_box():
     with open('bounding_box.csv', newline = '') as file:
         reader = csv.reader(file)
@@ -98,16 +97,62 @@ def create_earthquake_db(earthquakes, db_path='earthquakes.db'):
     """
     cursor.execute(create_sql)
 
+    cursor.execute("""
+    CREATE UNIQUE INDEX IF NOT EXISTS uq_earthquakes
+    ON earthquakes_db (day, time, mag, latitude, longitude, place);
+    """)
 
     conn.commit()
 
     insert_sql = (
-        "INSERT INTO earthquakes_db (day, time, mag, latitude, longitude, place) "
+        "INSERT OR IGNORE INTO earthquakes_db (day, time, mag, latitude, longitude, place) "
         "VALUES (?, ?, ?, ?, ?, ?)"
     )
 
 
     cursor.executemany(insert_sql, earthquakes)
+
+    cursor.execute("""
+    DELETE FROM earthquakes_db
+    WHERE rowid NOT IN (
+        SELECT MIN(rowid)
+        FROM earthquakes_db
+        GROUP BY day, time, mag, latitude, longitude, place
+    );
+    """)
+
     conn.commit()
     cursor.close()
     conn.close()
+
+
+def query_db(K, days, min_magnitude, db_path='earthquakes.db'):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    date_limit = (datetime.now() - timedelta(days=days)).strftime("%Y-%m-%d")
+
+    cursor.execute(
+        """
+        SELECT day, time, mag, latitude, longitude, place
+        FROM earthquakes_db
+        WHERE mag >= ?
+            AND day >= ?
+        ORDER BY mag DESC
+        LIMIT ?
+        """,
+        (min_magnitude, date_limit, K)
+    )
+
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+def print_earthquakes(earthquakes):
+    for eq in earthquakes:
+        day, time, mag, latitude, longitude, place = eq
+
+        print(
+            f"day: {day}, time: {time}, magnitude: {mag},\n"
+            f"lat: {latitude}, lon: {longitude}, place: {place}"
+        )
